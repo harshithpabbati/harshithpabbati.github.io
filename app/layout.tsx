38f695a5c8300c9c07a629dd57ce351571ad5360
harshithpabbati <pabbatiharshith@gmail.com>
2023-11-06 12:43:45 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./global.css";
 import clsx from "clsx";
 import type { Metadata } from "next";
 import { GeistSans, GeistMono } from "geist/font";
+import { GoogleAnalytics } from "components/GoogleAnalytics";
+import React from 'react';
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://harshithpabbati.com"),
@@ -51,6 +53,7 @@ export default function RootLayout({
     >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <GoogleAnalytics />
           {children}
         </main>
       </body>
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+	trailingComma: "es6",
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+};
+
+export default config;
