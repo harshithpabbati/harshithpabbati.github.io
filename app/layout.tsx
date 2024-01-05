0b63cbbc4e6600b0a5ebd6ddc452a9c754474c7a
harshithpabbati <pabbatiharshith@gmail.com>
2024-01-05 18:40:24 +0530

    chore: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { GeistSans, GeistMono } from "geist/font";
 import { GoogleAnalytics } from "components/GoogleAnalytics";
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://harshithpabbati.com"),
@@ -54,6 +55,7 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <GoogleAnalytics />
+          <Analytics />
           {children}
         </main>
       </body>
