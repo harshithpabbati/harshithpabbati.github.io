0e2b8c8d4191df2fee805d9178a67ca39e2220e8
harshithpabbati <pabbatiharshith@gmail.com>
2025-01-25 23:45:42 +0530

    Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist } from "next/font/google";
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { OpenpanelProvider } from '@openpanel/nextjs';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://harshithpabbati.com"),
@@ -60,6 +61,7 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Analytics />
+          <SpeedInsights />
           <OpenpanelProvider
             url="https://api.openpanel.dev"
             clientId="08f71e62-9642-406b-aa98-e9a6f95ec49b"
