d8caeee48fe23f920c59b6a1c8a0841baa3cb78e
harshithpabbati <pabbatiharshith@gmail.com>
2026-08-08 21:25:04 -0500

    chore: upgrade to vinext 1.0.0-beta.5 and vite 8

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import nextConfig from 'eslint-config-next';
 import prettierConfig from 'eslint-config-prettier';
 
 const config = [
+  { ignores: ["dist/**", ".vinext/**", "next-env.d.ts"] },
   ...(nextConfig && Array.isArray(nextConfig) ? nextConfig : nextConfig ? [nextConfig] : []),
   prettierConfig,
 ];
